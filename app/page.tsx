c174668dcfdd3a9a716ffd1e91439eaf1105b417
Ryoka <tenkaro@icloud.com>
2026-07-11 06:32:51 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/Hero";
 import ServicesBar from "@/components/ServicesBar";
-import AboutParagraph from "@/components/AboutParagraph";
 import BoardingPassTeaser from "@/components/BoardingPassTeaser";
+import AboutParagraph from "@/components/AboutParagraph";
 import JournalFeed from "@/components/JournalFeed";
 
 export default function Home() {
