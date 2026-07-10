3d6fd87b28857be4f3bc26a65b46a513414d4efc
Ryoka <tenkaro@icloud.com>
2026-07-11 06:29:53 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import ServicesBar from "@/components/ServicesBar";
 import AboutParagraph from "@/components/AboutParagraph";
+import BoardingPassTeaser from "@/components/BoardingPassTeaser";
 import JournalFeed from "@/components/JournalFeed";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <ServicesBar />
       <AboutParagraph />
+      <BoardingPassTeaser />
       <JournalFeed />
     </main>
   );
