c6bea3625e969daa33ce490c6abb4669b64f3f9a
Ryoka <tenkaro@icloud.com>
2026-07-15 09:31:59 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
       <Hero />
       <ServicesBar />
       <BoardingPassTeaser />
-      <AboutParagraph />
-      <PhotoStrip />
       <JournalFeed />
+      <PhotoStrip />
+      <AboutParagraph />
     </main>
   );
 }
